b09463ab1938f84b39b0a61d6f966ccc4ab98098
manigandanb02 <manigandanb02@gmail.com>
2026-06-17 19:47:06 +0530

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,7 @@
+"""
+PassProbe Modules Package
+
+Contains the core modules used by the Password Strength Analyzer,
+including password analysis, entropy calculation, password generation,
+history management, password comparison, and report generation.
+"""
